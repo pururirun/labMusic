6dc7dab2822dae4f600bf251ce96a47484f1fe29
meliveloz <mvelozcampos@gmail.com>
2018-01-03 15:55:24 -0300

    adjuntando carousel y conciertos

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+ 
+ for(i=0; i<concertData.length;i++){
+  $(".row-concert").append("<div class='col-lg-3'><div class='thumbnail modal-click'>"+concertData[i].photo+"<div class='caption'><h3>"+concertData[i].name+"</h3><p>"+concertData[i].date+"</p><p></p></div></div></div>");
+ }
+
+$(".modal-click").click(function(){ //al hacer click en el contenedor de las fotos
+  $("#myModal").modal(); //iniciando el modal desde javascript
+  var thisPhoto = $(this).children("img").attr("alt"); // guardamos la variable del Alt de la foto al hacer click
+  for(i=0 ; i< concertData.length; i++){ //recorremos la data
+    if (thisPhoto == concertData[i].name) { // si el alt de la foto es igual al nombre entonces...
+      $(".modal-titles").empty(); //borramos el titulo anterior
+      $(".modal-bodys").empty();
+      $(".modal-titles").html(concertData[i].name); // agregamos el nuevo nombre
+      $(".modal-bodys").append(concertData[i].photo+"<h4>Lugar :"+concertData[i].place+"</h4>");
+     
+    };
+
+};
+
+
+
+});
+
+
+});
---
+++ assets/js/data.js
@@ -0,0 +1,28 @@
+var concertData=[
+  {
+    name:"John Digweed & Guy J",
+    date:"05 de enero 2018",
+    place:"Espacio Sporting",
+    photo:"<img src='assets/images/concert/concert1.jpg' alt='John Digweed & Guy J' class='img-responsive'>"
+  },
+  {
+    name:"Godlevel Festival 2018",
+    date:"06 de enero 2018",
+    place:"Teatro Caupolicán",
+    photo:"<img src='assets/images/concert/concert2.jpg' alt='Godlevel Festival 2018' class='img-responsive'>"
+
+  },
+  {
+    name:"Festival Woodstaco 2018",
+    date:"12 de enero 2018",
+    place:"Cruce el Manzano – La Montaña de Teno",
+    photo:"<img src='assets/images/concert/concert3.jpg' alt='Festival Woodstaco 2018' class='img-responsive'>"
+  },
+  {
+    name:"Willie Colon",
+    date:"12 de enero 2018",
+    place:"Teatro Caupolicán",
+    photo:"<img src='assets/images/concert/concert4.jpg' alt='Willie Colon' class='img-responsive'>"
+  }
+
+];
