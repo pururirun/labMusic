f4d99fd40f2844c228afdbb09af3485044da1661
meliveloz <mvelozcampos@gmail.com>
2018-01-04 13:25:21 -0300

    firebase, registro y login

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,152 @@
+/* Inicio firebase registro e inicio*/
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE) //esto es para que se borre el usuario activo cada vez que inicio sesion
+  .then(function() {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  })
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+
+
+function register(){
+ var email = document.getElementById("email").value;
+ var password= document.getElementById("password").value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then(function(){
+    verificar();
+  })
+  .catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+  console.log(errorCode);
+  console.log(errorMessage);
+});
+};
+function verificar(){
+  var user = firebase.auth().currentUser;
+
+user.sendEmailVerification().then(function() {
+  // Email sent.
+  console.log("enviando correo");
+}).catch(function(error) {
+  // An error happened.
+  console.log(error);
+});
+};
+
+
+function ingreso(){
+  var email2 = document.getElementById("email2").value;
+  var password2= document.getElementById("password2").value;
+  firebase.auth().signInWithEmailAndPassword(email2, password2).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+
+
+ 
+});
+};
+
+
+function out(){
+  firebase.auth().signOut().then(function(){
+    console.log("saliendo..")
+    $(document).ready(function(){
+   
+  $('#modalLogin').modal('toggle'); 
+  $(".post-music").toggleClass("hidden");
+  $(".joinHeader").toggleClass("hidden");
+  $(".carousel-hidden").toggleClass("hidden");
+  $(".concert").toggleClass("hidden");
+  $(".userHeader").toggleClass("hidden");
+  $(".backgroundUser").toggleClass("hidden");
+  $(".artist").toggleClass("hidden");
+  $(".disc").toggleClass("hidden");
+  $(".listening").toggleClass("hidden");
+  var name= $(".name-user-input").val();
+  $("#name-user").html(name);
+  $("#user-name-perfil").html(name);
+
+$(".heart").click(function(){
+  $(this).toggleClass("red-toggle");
+
+
+});
+});
+  })
+  .catch(function(error){
+ console.log(error);
+  })
+};
+
+
+function observador(){
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("existe usuario activo");
+    // User is signed in.  
+    var user= user;
+  if(user.emailVerified){
+  
+ 
+  $('#modalLogin').modal('toggle');
+  $(".post-music").toggleClass("hidden"); 
+  $(".joinHeader").toggleClass("hidden");
+  $(".carousel-hidden").toggleClass("hidden");
+  $(".concert").toggleClass("hidden");
+  $(".userHeader").toggleClass("hidden");
+  $(".backgroundUser").toggleClass("hidden");
+  $(".artist").toggleClass("hidden");
+  $(".disc").toggleClass("hidden");
+  $(".listening").toggleClass("hidden");
+  var name= $(".name-user-input").val();
+  $("#name-user").html(name);
+  $("#user-name-perfil").html(name);
+
+$(".heart").click(function(){
+  $(this).toggleClass("red-toggle");
+});
+};
+
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+    // 
+    console.log("No existe usuario activo");
+  }
+});
+
+
+
+}
+observador();
+
+
+/*final firebase*/
+
+
+
 $(document).ready(function(){
+
  
  for(i=0; i<concertData.length;i++){
   $(".row-concert").append("<div class='col-lg-3'><div class='thumbnail modal-click'>"+concertData[i].photo+"<div class='caption'><h3>"+concertData[i].name+"</h3><p>"+concertData[i].date+"</p><p></p></div></div></div>");
@@ -20,25 +168,35 @@ $(".modal-click").click(function(){ //al hacer click en el contenedor de las fot
 
 });
 
-$(".btn-enter-user").click(function(){
-  $('#modalLogin').modal('toggle'); 
-  $(".joinHeader").toggleClass("hidden");
-  $(".carousel-hidden").toggleClass("hidden");
-  $(".concert").toggleClass("hidden");
-  $(".userHeader").toggleClass("hidden");
-  $(".backgroundUser").toggleClass("hidden");
-  $(".artist").toggleClass("hidden");
-  $(".disc").toggleClass("hidden");
-  $(".listening").toggleClass("hidden");
-  var name= $("#name-user-input").val();
-  $("#name-user").html(name);
-  $("#user-name-perfil").html(name);
-
-});
-$(".heart").click(function(){
-  $(this).toggleClass("red-toggle");
 
 
 });
+var idPin=0;
+
+$(document).ready(function(){
 
 });
+
+function savePin(){
+  var titulo = $("#tituloInput").val();
+  var descripcion = $("#descripcionInput").val();
+  var url = $("#urlInput").val();
+
+  $("#pines").append("<div id='pin_" + idPin + "' class='pin col-lg-6 col-lg-offset-3'>" +
+    //este es el frente
+    "<div class='panel panel-success front'>" +
+    "<div class='panel-heading'>" +
+    "<h3 class='panel-title'>" + titulo + "</h3>" +
+    "</div>"+
+    "<div class= 'panel-body'>" +
+    "<a href='#' class='thumbnail'>" +
+    "<img src='" + url + "' alt='...'>" +
+    "</a>" +
+    "</div>" +
+    "<div class ='panel-footer'>" + descripcion + "</div> </div>" + 
+    //este es el panel de atras
+    "<div class='rear'>"+
+    "</div>" +
+    "</div>");
+
+}
