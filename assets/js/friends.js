2c7b59b4d9593f4ee463c822c089ca4614c8c394
meliveloz <mvelozcampos@gmail.com>
2018-01-05 03:14:11 -0300

    click en amigos, contador de amigos, data amigos

---
+++ assets/js/friends.js
@@ -0,0 +1,44 @@
+var friends=[
+  {
+    name:"Juan Perez",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+  },
+
+  {
+    name:"Pepito",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+
+  },
+  {
+    name:"Elias Jarpa",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+  },
+  {
+    name:"Sofia Lopéz",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+  },
+  {
+    name:"Loreto Ponce",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+  },
+  {
+    name:"Jose Cid",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+
+  },
+  {
+    name:"Oliver Pardo",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+  },
+  {
+    name:"Olga Campos",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+  },
+  {
+    name:"Carla Ramos",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+
+  }
+
+];
+
---
+++ assets/js/sugerencia.js
@@ -0,0 +1,46 @@
+var suggestion= [
+  {
+    name:"Camila Olmos",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+  },
+
+  {
+    name:"Roberto Pinto",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+
+  },
+  {
+    name:"Lucia Henriquez",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+  },
+  {
+    name:"Patricio sanchez",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+  },
+  {
+    name:"Felipe Bozo",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+  },
+  {
+    name:"Lorena Parra",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+
+  },
+  {
+    name:"Alvaro Pando",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+  },
+  {
+    name:"Cecilia Ramos",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+  },
+  {
+    name:"Florencia Cid",
+    photo:"<img class='imagen media-object dp img-circle' src='assets/images/raccoon.jpg' style='width: 100px;height:100px;'>"
+
+  }
+
+
+
+
+];
